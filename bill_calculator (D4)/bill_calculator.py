234fd5b7e9cd0866082d14c672a4d20aca988df6
Harsh Kadam <harshlk11101@gmail.com>
2026-01-07 22:13:56 +0530

    Day 4 - aws_bill calculator done

---
+++ bill_calculator (D4)/bill_calculator.py
@@ -0,0 +1,18 @@
+import json
+
+with open('aws_services.json','r') as f:
+    data = json.load(f)
+    total = 0
+
+    for service in data['services']:
+
+        total += service['hours'] * service['rate_per_hour']
+
+    if total < data['budget_limit']:
+        print(f"Services are within budget")
+    else:
+        print(f"ALERT : Total cost {total} exceeds budget of {data['budget_limit']}")
+
+
+
+
