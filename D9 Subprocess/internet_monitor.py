be6404c38082a27db94f0f60b21f5e9ecb8c6139
Harsh Kadam <harshlk11101@gmail.com>
2026-01-13 19:41:00 +0530

    Day-10 edited internet monitor script

--- D9 Subprocess/internet_monitor.py
+++ D9 Subprocess/internet_monitor.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python3
 import subprocess
 import sys
+from datetime import datetime
 
 if len(sys.argv)<2 :
     print("Error : Enter the address")
-    sys.exit(1);
+    sys.exit(1)
 
 address = sys.argv[1]
+timestamp =  datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-result = subprocess.run(["ping","-c","1",address])
+result = subprocess.run(["ping","-c","1",address],capture_output=True,text=True)
 
 if result.returncode==0:
+    status = "Up"
     print(f"{address} is up")
 else:
+    status = "Down"
     print(f"{address} is down")
+
+with open("monitor.log","a") as f:
+    f.write(f"[{timestamp}] Target : {address} | Status:{status}")
+    print("Saved to monitor.log")
